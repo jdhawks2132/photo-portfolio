9725938a5190ffe2fe475e76641ce4046a0eb2ce
Josh Hawks <snl95@txstate.edu>
2022-11-18 14:13:46 -0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // import pages
 import About from './pages/about/About';
 
+// import components
+import Navbar from './components/nav/Navbar';
+
 function App() {
 	return (
 		<div>
+			<Navbar />
 			<main>
 				<About />
 			</main>

94b650bfc6abd6039a7797391459bdf77fe69f8e
Josh Hawks <snl95@txstate.edu>
2022-11-18 15:40:47 -0600

    add testing coverage to gallery component

---
+++ src/pages/gallery/__tests__/gallery.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Gallery from '../Gallery';
+
+afterEach(cleanup);
+
+const portrait = {
+	name: 'portraits',
+	description: 'Portraits of people in my life',
+};
+
+describe('Gallery', () => {
+	it('renders', () => {
+		render(<Gallery currentCategory={portrait} />);
+	});
+
+	it('matches snapshot DOM node structure', () => {
+		const { asFragment } = render(<Gallery currentCategory={portrait} />);
+		expect(asFragment()).toMatchSnapshot();
+	});
+});
